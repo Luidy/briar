06998e04ba3553db3ad8e339d01881b45bb4b66c
DESKTOP-7CDDM0K\ejjo3 <ejjo321@gmail.com>
2021-03-30 14:58:56 +0900

    add. grpc server start code

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module ges
 go 1.13
 
 require (
+	github.com/grpc-ecosystem/go-grpc-middleware v1.0.0
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.7.1
+	google.golang.org/grpc v1.21.1
 )
--- main.go
+++ main.go
@@ -2,30 +2,36 @@ package main
 
 import (
 	"fmt"
-	"runtime"
 	"ges/config"
+	"log"
+	"net"
+	"os"
+	"runtime"
+
+	"github.com/spf13/viper"
+	"google.golang.org/grpc"
 )
 
 const (
-	banner = "\n" +   
-                                                              
-"        GGGGGGGGGGGGGEEEEEEEEEEEEEEEEEEEEEE   SSSSSSSSSSSSSSS   \n"+
-"     GGG::::::::::::GE::::::::::::::::::::E SS:::::::::::::::S  \n"+
-"   GG:::::::::::::::GE::::::::::::::::::::ES:::::SSSSSS::::::S  \n"+
-"  G:::::GGGGGGGG::::GEE::::::EEEEEEEEE::::ES:::::S     SSSSSSS  \n"+
-" G:::::G       GGGGGG  E:::::E       EEEEEES:::::S              \n"+
-"G:::::G                E:::::E             S:::::S              \n"+
-"G:::::G                E::::::EEEEEEEEEE    S::::SSSS           \n"+
-"G:::::G    GGGGGGGGGG  E:::::::::::::::E     SS::::::SSSSS      \n"+
-"G:::::G    G::::::::G  E:::::::::::::::E       SSS::::::::SS    \n"+
-"G:::::G    GGGGG::::G  E::::::EEEEEEEEEE          SSSSSS::::S   \n"+
-"G:::::G        G::::G  E:::::E                         S:::::S  \n"+
-" G:::::G       G::::G  E:::::E       EEEEEE            S:::::S  \n"+
-"  G:::::GGGGGGGG::::GEE::::::EEEEEEEE:::::ESSSSSSS     S:::::S  \n"+
-"   GG:::::::::::::::GE::::::::::::::::::::ES::::::SSSSSS:::::S  \n"+
-"     GGG::::::GGG:::GE::::::::::::::::::::ES:::::::::::::::SS   \n"+
-"        GGGGGG   GGGGEEEEEEEEEEEEEEEEEEEEEE SSSSSSSSSSSSSSS   \n\n"
-                                                              
+	banner = "\n" +
+
+		"        GGGGGGGGGGGGGEEEEEEEEEEEEEEEEEEEEEE   SSSSSSSSSSSSSSS   \n" +
+		"     GGG::::::::::::GE::::::::::::::::::::E SS:::::::::::::::S  \n" +
+		"   GG:::::::::::::::GE::::::::::::::::::::ES:::::SSSSSS::::::S  \n" +
+		"  G:::::GGGGGGGG::::GEE::::::EEEEEEEEE::::ES:::::S     SSSSSSS  \n" +
+		" G:::::G       GGGGGG  E:::::E       EEEEEES:::::S              \n" +
+		"G:::::G                E:::::E             S:::::S              \n" +
+		"G:::::G                E::::::EEEEEEEEEE    S::::SSSS           \n" +
+		"G:::::G    GGGGGGGGGG  E:::::::::::::::E     SS::::::SSSSS      \n" +
+		"G:::::G    G::::::::G  E:::::::::::::::E       SSS::::::::SS    \n" +
+		"G:::::G    GGGGG::::G  E::::::EEEEEEEEEE          SSSSSS::::S   \n" +
+		"G:::::G        G::::G  E:::::E                         S:::::S  \n" +
+		" G:::::G       G::::G  E:::::E       EEEEEE            S:::::S  \n" +
+		"  G:::::GGGGGGGG::::GEE::::::EEEEEEEE:::::ESSSSSSS     S:::::S  \n" +
+		"   GG:::::::::::::::GE::::::::::::::::::::ES::::::SSSSSS:::::S  \n" +
+		"     GGG::::::GGG:::GE::::::::::::::::::::ES:::::::::::::::SS   \n" +
+		"        GGGGGG   GGGGEEEEEEEEEEEEEEEEEEEEEE SSSSSSSSSSSSSSS   \n\n" +
+		"=> Starting gRPC server %s\n\n"
 )
 
 func init() {
@@ -35,5 +41,27 @@ func init() {
 
 func main() {
 	ges := config.Ges
-	fmt.Println(banner,	ges)
+	g := grpcInit()
+	startServer(ges, g)
+}
+
+func grpcInit() (g *grpc.Server) {
+	g = grpc.NewServer()
+	return g
+}
+
+func startServer(ges *viper.Viper, g *grpc.Server) {
+	address := fmt.Sprintf("0.0.0.0:%d", ges.GetInt("port"))
+	fmt.Printf(banner, address)
+
+	l, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Println("End to bind for gRPC server", "err", err)
+		os.Exit(1)
+	}
+	if err := g.Serve(l); err != nil {
+		log.Println("End gRPC server", "err", err)
+		os.Exit(1)
+	}
+
 }
